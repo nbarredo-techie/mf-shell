736019e41a89b3f8f8b534b85463db19c6b6b324
nbarredo <nbarredo@techietalent.net>
2025-05-14 09:42:07 -0300

    intitial

---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import singleSpa from 'vite-plugin-single-spa';
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    singleSpa({ type: 'mife', serverPort: 5173, spaEntryPoints: './src/root.component.tsx' }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    port: 5173,
+    cors: {
+      origin: '*',
+      methods: ['GET'],
+    }
+  },
+  build: {
+    target: 'esnext',
+    modulePreload: false,
+    minify: false,
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        format: 'system',
+        entryFileNames: 'mf-shell.js',
+      },
+    },
+  },
+});
