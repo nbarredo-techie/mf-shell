fa3e0f72b5957e101bccd3053cd77e62b4f4f04b
nbarredo <nbarredo@techietalent.net>
2025-05-14 16:05:55 -0300

    remove

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react-swc';
 import singleSpa from 'vite-plugin-single-spa';
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
+import federation from '@originjs/vite-plugin-federation';
+
 
 export default defineConfig({
   plugins: [
@@ -13,7 +15,27 @@ export default defineConfig({
       serverPort: 5173, 
       spaEntryPoints: './src/main.tsx'  // Changed to main.tsx which has the single-spa lifecycle functions
     }),
-  ],
+    federation({
+      name: 'mf-shell',
+      remotes: {
+        shared_ui: 'https://terrabostmanagestorage.blob.core.windows.net/$web/shared-ui/remoteEntry.js', // Update URL to match your deployment
+      },
+      shared: {
+        react: { 
+          requiredVersion: '^19.0.0',
+          import: false
+        },
+        'react-dom': { 
+          requiredVersion: '^19.0.0',
+          import: false
+        }, 
+        'tailwindcss': {
+          requiredVersion: '^4.0.0',
+          import: false
+        } 
+      }
+    })
+  ], 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -31,7 +53,7 @@ export default defineConfig({
     modulePreload: false,
     minify: false,
     cssCodeSplit: false,
-    rollupOptions: {
+    rollupOptions: { 
       output: {
         format: 'system',
         entryFileNames: 'mf-shell.js',
