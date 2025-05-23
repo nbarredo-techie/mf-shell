008f665effa2ab8eb4b9aeeb3ab61eb7c04e73a0
nbarredo <nbarredo@techietalent.net>
2025-05-23 17:11:15 -0300

    back

--- webpack.config.js
+++ webpack.config.js
@@ -2,43 +2,20 @@ const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
 
 module.exports = (webpackConfigEnv, argv) => {
-  const isLocal = webpackConfigEnv && (webpackConfigEnv.isLocal || webpackConfigEnv.isDev);
-  // Ensure webpackConfigEnv.isDev is correctly set based on argv.mode
-  // This helps singleSpaDefaults configure HMR and output formats correctly for development
-  const effectiveWebpackConfigEnv = {
-    ...webpackConfigEnv,
-    isDev: argv.mode === 'development',
-  };
-
   const defaultConfig = singleSpaDefaults({
     orgName: "terraboost",
     projectName: "mf-shell",
-    webpackConfigEnv: effectiveWebpackConfigEnv, // Use the corrected env
+    webpackConfigEnv,
     argv,
-    outputSystemJS: false, // Recommended to be false for Module Federation
+    outputSystemJS: false,
   });
 
   return merge(defaultConfig, {
-    // modify the webpack config however you'd like to by adding to this object
-    externals: ['shared-ui', 'react', 'react-dom', 'react-dom/client'], // Reverted externals
-    devServer: {
-      ...defaultConfig.devServer,
-      hot: true,
-      port: 8080,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env', '@babel/preset-react'],
-            },
-          },
-        },
-      ],
-    },
+    ...defaultConfig,
+    // Explicitly define externals, especially if outputSystemJS is false
+    externals: [
+      "react",
+      "react-dom",
+    ],
   });
 };
