8177c4e215f8788ea3d174049659fee38c54e831
nbarredo <nbarredo@techietalent.net>
2025-05-23 12:49:56 -0300

    mod fed

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
+const webpack = require('webpack'); // Import webpack
 
 module.exports = (webpackConfigEnv, argv) => {
   // Ensure webpackConfigEnv.isDev is correctly set based on argv.mode
@@ -19,10 +20,37 @@ module.exports = (webpackConfigEnv, argv) => {
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
-    externals: ['shared-ui', 'react', 'react-dom'], // Added react and react-dom
+    externals: ['shared-ui'], // react and react-dom will be federated
     devServer: {
       hot: true, // Ensure HMR is enabled for webpack-dev-server
       port: 8080, // Set fixed port to 8080
     },
+    plugins: [
+      new webpack.container.ModuleFederationPlugin({
+        name: "mf_shell",
+        filename: "remoteEntry.js",
+        remotes: {
+          root_config: "root_config@http://localhost:3000/remoteEntry.js",
+        },
+        shared: {
+          ...defaultConfig.externals,
+          react: {
+            singleton: true,
+            requiredVersion: false,
+          },
+          "react-dom": {
+            singleton: true,
+            requiredVersion: false,
+          },
+          "react-dom/client": {
+            singleton: true,
+            requiredVersion: false,
+          },
+          "shared-ui": {
+            singleton: true,
+          }
+        },
+      }),
+    ],
   });
 };
