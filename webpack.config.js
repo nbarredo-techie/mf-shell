942ef7434e7f993fe53c6bcc60d2da99d8acf159
nbarredo <nbarredo@techietalent.net>
2025-05-23 10:23:27 -0300

    adding react deps

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,26 @@ const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
 
 module.exports = (webpackConfigEnv, argv) => {
+  // Ensure webpackConfigEnv.isDev is correctly set based on argv.mode
+  // This helps singleSpaDefaults configure HMR and output formats correctly for development
+  const effectiveWebpackConfigEnv = {
+    ...webpackConfigEnv,
+    isDev: argv.mode === 'development',
+  };
+
   const defaultConfig = singleSpaDefaults({
     orgName: "terraboost",
     projectName: "mf-shell",
-    webpackConfigEnv,
+    webpackConfigEnv: effectiveWebpackConfigEnv, // Use the corrected env
     argv,
     outputSystemJS: false,
   });
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
-    externals: ['shared-ui'],
+    externals: ['shared-ui', 'react', 'react-dom'], // Added react and react-dom
+    devServer: {
+      hot: true, // Ensure HMR is enabled for webpack-dev-server
+    },
   });
 };
