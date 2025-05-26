007614338eab3c9b296546a9a5391c737f2ddbfa
nbarredo <nbarredo@techietalent.net>
2025-05-26 16:00:42 -0300

    fixing routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,13 @@
 export const routes = [
-    { path: '/dashboard', label: 'Dashboard11111' },
-    { path: '/kiosks', label: 'Kiosks' },
-    { path: '/venues', label: 'Venues' },
-    { path: '/art', label: 'Art & Creative' },
-    { path: '/payments', label: 'Payments' },
-    { path: '/workorders', label: 'Work Orders' },
-    { path: '/company', label: 'Company / Customer' },
-    { path: '/admin', label: 'Admin / Users' },
-    { path: '/chat', label: 'Chat & Messaging' },
-    { path: '/reports', label: 'Reporting' },
+    { url: '/dashboard', title: 'Dashboard', id: 1 },
+    { url: '/kiosks', title: 'Kiosks', id: 2 },
+    { url: '/venues', title: 'Venues', id: 3 },
+    { url: '/art', title: 'Art & Creative', id: 4 },
+    { url: '/payments', title: 'Payments', id: 5 },
+    { url: '/workorders', title: 'Work Orders', id: 6 },
+    { url: '/company', title: 'Company / Customer', id: 7 },
+    { url: '/admin', title: 'Admin / Users', id: 8 },
+    { url: '/chat', title: 'Chat & Messaging', id: 9 },
+    { url: '/reports', title: 'Reporting', id: 10 },
   ];
   
