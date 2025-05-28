21cf2550398843c77771001f5a66ea4e20c69963
nbarredo <nbarredo@techietalent.net>
2025-05-28 11:02:15 -0300

    adding hashes

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,12 @@
 export const routes = [
-    { url: '/dashboard', title: 'Dashboard', id: 1 },
-    { url: '/kiosks', title: 'Kiosks', id: 2 },
-    { url: '/venues', title: 'Venues', id: 3 },
-    { url: '/art', title: 'Art & Creative', id: 4 },
-    { url: '/payments', title: 'Payments', id: 5 },
-    { url: '/workorders', title: 'Work Orders', id: 6 },
-    { url: '/company', title: 'Company / Customer', id: 7 },
-    { url: '/admin', title: 'Admin / Users', id: 8 },
-    { url: '/chat', title: 'Chat & Messaging', id: 9 },
-    { url: '/reports', title: 'Reporting', id: 10 },
-  ];
-  
+  { url: "#/dashboard", title: "Dashboard", id: 1 },
+  { url: "#/kiosks", title: "Kiosks", id: 2 },
+  { url: "#/venues", title: "Venues", id: 3 },
+  { url: "#/art", title: "Art & Creative", id: 4 },
+  { url: "#/payments", title: "Payments", id: 5 },
+  { url: "#/workorders", title: "Work Orders", id: 6 },
+  { url: "#/company", title: "Company / Customer", id: 7 },
+  { url: "#/admin", title: "Admin / Users", id: 8 },
+  { url: "#/chat", title: "Chat & Messaging", id: 9 },
+  { url: "#/reports", title: "Reporting", id: 10 },
+];
